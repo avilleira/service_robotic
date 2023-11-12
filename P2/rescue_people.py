ce936a69c3175e2d68f23b41e7ab2b3d9656d6a4
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-11-12 20:00:06 +0100

    Patrolling finished
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- P2/rescue_people.py
+++ P2/rescue_people.py
@@ -1,14 +1,28 @@
 from GUI import GUI
 from HAL import HAL
+import math
+import time
 
 
 # ----------- GLOBAL VARIABLES ------------------
 UTM_INIT = [430492, 4459162] # UTM: East and North
 UTM_SURV_POSE = [430532, 4459132]
+
+# STATES
 TAKE_OFF = 0
 PATROLLING = 1
-HEIGHT = 2 #m
 
+#DATA
+HEIGHT = 2 #m
+SURVIVORS = 6
+TIME_LIMIT = 600 # 10 min
+SQUARE_AREA = 17 #m
+TURN_LENGTH = 1
+
+# DIRECTIONS
+SOUTH = 1
+EAST = 2
+WEST = 3
 
 def mv_survivors_init_pose ():
   # Calculating survivors position using UTM
@@ -24,25 +38,72 @@ def mv_survivors_init_pose ():
     GUI.showLeftImage(HAL.get_ventral_image())
     GUI.showImage(HAL.get_frontal_image())
   
+
+def rotate_yaw (angle):
+  prev_pos = HAL.get_position()
+  # Turn in yaw axe 
+  pos = HAL.get_position()
+  while (round(current_pos[2]) != angle):
+    HAL.set_cmd_pos(pos[0], pos[1], HEIGHT, angle)
+    GUI.showLeftImage(HAL.get_ventral_image())
+    GUI.showImage(HAL.get_frontal_image())
+
   
 # Enter sequential code!
 
 state = TAKE_OFF
+direction = WEST
+prev_dir = 0
+posx = 0
+posy = 0
 
+#Init time MAX 10 min
+init_time = time.time()
 while True:
     # Enter iterative code
     # Taking off state
     if state == TAKE_OFF:
       HAL.takeoff(HEIGHT)
       mv_survivors_init_pose()
-      init_point = HAL.get_position()
+      init_square_point = HAL.get_position()
+      print("SALGO DE TAKE_OFF")
       state = PATROLLING
       
     elif state == PATROLLING:
-      HAL.set_cmd_mix(0, 1, 2, 0)
       current_pos = HAL.get_position()
-      if current_pos[1] >= init_point[1] + 20:
-        print("HE LLEGAO")
+      speedx = 0
+      speedy = 0
+      # Changing direnctions
+      if direction == SOUTH:
+        speedx = 0
+        speedy = -0.5
+        
+        if current_pos[1] <= posy - TURN_LENGTH:
+          if prev_dir == WEST:
+            direction = EAST
+          elif prev_dir == EAST:
+            direction = WEST
+            
+      elif direction == WEST:
+        speedx = -1.5
+        speedy = 0
+        
+        if current_pos[0] <= init_square_point[0] - init_square_point:
+          prev_dir = direction
+          direction = SOUTH
+          posy = HAL.get_position()[1]
+        
       
+      elif direction == EAST:
+        speedx = 1.5
+        speedy = 0
+        
+        if current_pos[0] >= init_square_point[0] + 2:
+          prev_dir = direction
+          direction = SOUTH
+          posy = HAL.get_position()[1]
+      
+      HAL.set_cmd_mix(speedx, speedy, HEIGHT, 0)  
+
     GUI.showLeftImage(HAL.get_ventral_image())
     GUI.showImage(HAL.get_frontal_image())

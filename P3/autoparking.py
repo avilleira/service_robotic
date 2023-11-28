1b2e931b77228ec5b70e4cf4efe856b1ec4bf182
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-11-28 02:31:51 +0100

    Trying to detect free space
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- P3/autoparking.py
+++ P3/autoparking.py
@@ -1,16 +1,18 @@
 from GUI import GUI
 from HAL import HAL
+
 # Enter sequential code!
 
 def detect_free_spcace():
-  f_laser = HAL.getFrontLaserData()
-  r_laser = HAL.getRightLaserData()
-  b_laser = HAL.getBackLaserData()
+  f_laser = HAL.getFrontLaserData().values
+  r_laser = HAL.getRightLaserData().values
+  b_laser = HAL.getBackLaserData().values
+  
 
 while True:
     # Enter iterative code!
     front_laser = HAL.getFrontLaserData()
     right_laser = HAL.getRightLaserData()
     back_laser = HAL.getBackLaserData()
-    print(right_laser.minAngle)
     HAL.setV(0.5)
+    detect_free_spcace()

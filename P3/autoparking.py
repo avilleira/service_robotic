048172de0fda691c2021ab46565299715897d605
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-12-03 13:19:16 +0100

    Car free space founded, need to align with the next car
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- P3/autoparking.py
+++ P3/autoparking.py
@@ -3,15 +3,23 @@ from HAL import HAL
 
 # Enter sequential code!
 
+# MACROS
+
+SEARCHING = 0
+ALIGNING = 1
+PARKING = 2
+
+front_right_laser = 60
+
+
 def detect_free_spcace():
   f_laser = HAL.getFrontLaserData().values
   r_laser = HAL.getRightLaserData().values
   b_laser = HAL.getBackLaserData().values
-  
   free_space = False
 
   for angle in range(len(r_laser)):
-    if angle >= 40 and angle <= 125:
+    if angle >= front_right_laser and angle <= 140:
       if r_laser[angle] > HAL.getRightLaserData().maxRange:
         free_space = True
       else:
@@ -19,12 +27,33 @@ def detect_free_spcace():
         return free_space
   
   return free_space
+  
+  
+def line_up_car():
+  r_laser = HAL.getRightLaserData().values
+  for angle in range(len(r_laser)):
+    if angle >= 50 and angle <= 130:
+      if r_laser[angle] < HAL.getRightLaserData().minRange:
+        free_space = True
+      else:
+        free_space = False
+        return free_space
+  return True
+  
+  
+
+status = SEARCHING
 
 while True:
     # Enter iterative code!
-
-    if detect_free_spcace() == True:
-      HAL.setV(-0.5)
-      print("HUECO ENCONTRADO")
-    else:
-      HAL.setV(0.5)
+    if status == SEARCHING:
+      if detect_free_spcace() == True:
+        HAL.setV(0)
+        print("HUECO ENCONTRADO")
+        status = PARKING
+      else:
+        HAL.setV(1)
+    
+    elif status == ALIGNING:
+      line_up_car()
+      

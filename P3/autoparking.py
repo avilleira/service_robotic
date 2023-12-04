c4ab9bf99431429cc57dd31516e438309aceea56
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-12-05 00:06:33 +0100

    Car can Park
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- P3/autoparking.py
+++ P3/autoparking.py
@@ -10,6 +10,7 @@ MINDISTANCE = 5
 SEARCHING = 0
 ALIGNING = 1
 PARKING = 2
+PARKED = 3
 
 # MOVEMENTS
 LEFT_BACK = 0
@@ -18,16 +19,15 @@ FRONT = 2
 RIGHT_BACK = 3
 
 front_right_laser = 60
+back_right_laser = 120
 
 
-def detect_free_spcace():
-  f_laser = HAL.getFrontLaserData().values
+def detect_free_space():
   r_laser = HAL.getRightLaserData().values
 
   free_space = False
-
   for angle in range(len(r_laser)):
-    if angle >= front_right_laser and angle <= 140:
+    if angle >= front_right_laser and angle <= back_right_laser:
       if r_laser[angle] > HAL.getRightLaserData().maxRange:
         free_space = True
       else:
@@ -39,22 +39,19 @@ def detect_free_spcace():
   
 def line_up_car():
   r_laser = HAL.getRightLaserData().values
+  
   for angle in range(len(r_laser)):
-    if angle >= 50 and angle <= 130:
-      if r_laser[angle] < MINDISTANCE:
-        free_space = True
-      else:
-        free_space = False
-        return free_space
+    if angle >= 30 and angle <= 90:
+      if r_laser[angle] > MINDISTANCE:
+        return False
   return True
   
-def distance_behind():
-  b_laser = HAL.getBackLaserData().values
   
+def distance_laser(laser):
   min_val = 100
   
-  for angle_value in b_laser:
-    if angle_value < min_val and angle_value > HAL.getRightLaserData().minRange:
+  for angle_value in laser.values:
+    if angle_value < min_val and angle_value > laser.minRange:
       min_val = angle_value
   
   return min_val
@@ -66,12 +63,10 @@ left_back = False
 init_ori = HAL.getPose3d().yaw
 
 while True:
-
     # Enter iterative code!
     if status == SEARCHING:
-      if detect_free_spcace() == True:
+      if detect_free_space() == True:
         HAL.setV(0)
-        print("HUECO ENCONTRADO")
         status = ALIGNING
       else:
         HAL.setV(1)
@@ -81,7 +76,6 @@ while True:
       if line_up_car() == True:
         HAL.setV(0)
         status = PARKING
-        print("ALINEADO")
         # We keep current orientation
         init_ori = HAL.getPose3d().yaw
       else:
@@ -89,21 +83,31 @@ while True:
         
     elif status == PARKING:
       if movement == LEFT_BACK:
-        if ((HAL.getPose3d().yaw - init_ori)*180/math.pi) <= 50:
-          HAL.setW(0.5)
+        print(distance_laser(HAL.getBackLaserData()))
+        if ((HAL.getPose3d().yaw - init_ori)*180/math.pi) <= 45:
+          HAL.setW(1)
           HAL.setV(-0.5)
         else:
           movement = RIGHT_BACK
         
       elif movement == RIGHT_BACK:
-        if ((HAL.getPose3d().yaw() - init_ori)*180/math.pi) > 0:
+        if ((HAL.getPose3d().yaw - init_ori)*180/math.pi) > 0:
           HAL.setW(-1)
           HAL.setV(-0.5)
           
-          if distance_behind() < 3:
-            if round(init_ori - HAL.getPose3d.yaw()) == 0: 
-              HAL.setV(1)
+          if distance_laser(HAL.getBackLaserData()) < 0.8:
+            movement = FRONT
         else:
+          status = PARKED
+          HAL.setV(0)
+          HAL.setW(0)
+          
+      elif movement == FRONT:
+        if distance_laser(HAL.getBackLaserData()) > 3 or distance_laser(HAL.getFrontLaserData()) < 1:
           movement = RIGHT_BACK
-        
-    
+        else:
+          HAL.setV(0.5)
+          HAL.setW(-1)
+    elif status == PARKED:
+      HAL.setV(0)
+      HAL.setW(0)
